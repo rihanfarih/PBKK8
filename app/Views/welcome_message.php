145a19d9bdcfcb06fc167eda1a7c3638fee741a2
rihanfarih <55140308+rihanfarih@users.noreply.github.com>
2022-04-18 20:58:44 +0700

    commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,168 @@
+<html>
+<title>Proilenya Rihan -PBKK 8</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+ 
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+ 
+<!-- The Grid -->
+<div class="w3-row-padding">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+ 
+<!-- Left Column -->
+<div class="w3-third">
+ 
+<div class="w3-white w3-text-grey w3-card-4">
+<div class="w3-display-container">
+<img src="https://2.bp.blogspot.com/-eJIVTAHnxVo/YS2kZ12TiiI/AAAAAAAAAfA/JnByYdoIVM4AmvqLqDY8gGMMSYcK22NLACK4BGAYYCw/s120-pf/fotorihan.jpg" style="width:100%" alt="Avatar">
+<div class="w3-container w3-text-black">
+<h2>Rihan Farih Bunyamin</h2>
+</div>
+</div>
+<div class="w3-container">
+<p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-teal"></i>Informatika ITS 2019</p>
+<p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Semarang</p>
+<p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>rihan.farih@gmail.com</p>
+<p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>08112930501</p>
+<hr>
+ 
+<br>
+</div>
+</div><br>
+ 
+<!-- End Left Column -->
+</div>
+ 
+<!-- Right Column -->
+<div class="w3-twothird">
+ 
+<div class="w3-container w3-card w3-white w3-margin-bottom">
+	<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>Institut Teknologi Sepuluh Nopember</b></h5>
+		<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2019-Now</h6>
+		<p>Informatics Bachelor Degree</p>
+		<hr>
+	</div>
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>SMA Negeri 3 Semarang</b></h5>
+		<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2016 - 2019</h6>
+		<p>Highschool</p>
+		<hr>
+	</div>
+</div>
+ 
+<div class="w3-container w3-card w3-white w3-margin-bottom">
+	<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Organisasi</h2>
+
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>Staff Ahli kewiraushaaan</b></h5>
+		<h6 class="w3-opacity"><b>Himpunan Mahasiswa Teknik Computer - Informatika ITS</b></h6>
+		<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021 - Now</h6>
+		
+		<hr>
+	</div>
+
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>Institut Teknologi Sepuluh Nopember</b></h5>
+		<h7 class="w3-opacity"><b>Assistant Lectrurer for Liniear Algebra</b></h7>
+		<br>
+		<h7 class="w3-opacity"><b>Assistant Lectrurer for Numerical Method</b></h7>
+		<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Aug 2021 - Des 2021</h6>
+		
+		<hr>
+	</div>
+
+
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>Anggota (Members)</b></h5>
+		<h6 class="w3-opacity"><b>IEEE ITS Student Branch</b></h6>
+		<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2021 - Now</h6>
+		
+		<hr>
+	</div>
+
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>Anggota Muda (AMDA)</b></h5>
+		<h6 class="w3-opacity"><b>UKM TDC ITS</b></h6>
+		<h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2019 - 2020</h6>
+		
+		<hr>
+	</div>
+</div>
+
+<div class="w3-container w3-card w3-white w3-margin-bottom">
+	<h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Pelatihan</h2>
+
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>AWS Academy Graduate</b></h5>
+		<h7 class="w3-opacity"><b>AWS Academy Cloud Architecting</b></h7>
+		<br>
+		<h7 class="w3-opacity"><b>AWS Academy Data Analytics</b></h7>
+		
+		
+		<hr>
+	</div>
+
+	<div class="w3-container">
+		<h5 class="w3-opacity"><b>Data Science Math Skills</b></h5>
+		<h6 class="w3-opacity"><b>Coursera</b></h6>
+		
+		
+		<hr>
+	</div>
+
+	
+</div>
+
+
+ 
+
+
+
+<!-- End Grid -->
+</div>
+ 
+<!-- End Page Container -->
+</div>
+ 
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+<p>Find me on social media</p>
+<a href="https://github.com/rihanfarih">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-github" viewBox="0 0 16 16">
+                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+              </svg>
+            </a>
+            <a href="mailto:rihan.farih@gmail.com">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-mailbox" viewBox="0 0 16 16">
+                <path d="M4 4a3 3 0 0 0-3 3v6h6V7a3 3 0 0 0-3-3zm0-1h8a4 4 0 0 1 4 4v6a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7a4 4 0 0 1 4-4zm2.646 1A3.99 3.99 0 0 1 8 7v6h7V7a3 3 0 0 0-3-3H6.646z"/>
+                <path d="M11.793 8.5H9v-1h5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.354-.146l-.853-.854zM5 7c0 .552-.448 0-1 0s-1 .552-1 0a1 1 0 0 1 2 0z"/>
+              </svg>
+            </a>
+              <a href="https://www.linkedin.com/in/rihan-farih-bunyamin-69158a1ba/">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-linkedin" viewBox="0 0 16 16">
+                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+              </svg>
+            </a>
+              <a href="https://www.instagram.com/rihanfarih/">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-instagram" viewBox="0 0 16 16">
+                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                </svg>
+               </a>
+<p>Created with <i class="bi bi-heart-fill text-danger"></i> by <a href="https://rihanfarih.blogspot.com/" class="text-white font-weight-bold">Rihan Farih</a></p>
+<p>Visit <a href="https://tugasadalahmakanankeseharianku.blogspot.com" target="_blank">my blog!^^</a></p>
+</footer>
+ 
+</body>
+</html>
